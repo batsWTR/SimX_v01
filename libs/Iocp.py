b9e697b1668d4f4425bbb6aa0aead00eb988a126
Baptiste <baptiste.wentzler@wanadoo.fr>
2020-04-15 18:06:45 +0200

    Iocp v2 class

--- libs/Iocp.py
+++ libs/Iocp.py
@@ -2,7 +2,7 @@
 # First, connect to server with the "Connect" function with IP address an Port (8090)
 # Register the list of all variable you wwish to receive with "Register" function with the list of variables 
 # as parameter.
-# "RecvData" function allow you to receive an update in case of variable change as a dict: [number][var]
+# "Recv" function allow you to receive an update in case of variable change as a dict: [number][var]
 # "Send" function allow you to change a variable, it send a dict to the class [number][var]
 # Close function will close the connection to the server
 #		IOCP Protocol
@@ -14,25 +14,25 @@
 
 
 
-
+# IOCP ver 2 2020
 
 # written by B.Wentzler baptiste.wentzler@wanadoo.fr
 
 
 import socket
+import time
 
 
 
 class Iocp:
 	""" This class is used to connect to an UIPC server"""
 
-	def __init__(self, ip = None, port = None):
-		self.IP = ip
-		self.Port = port
+	def __init__(self,IP = "", Port = 0):
+		self.IP = IP
+		self.Port = Port
 		self.s  = 0
 		self.listRegister = list()
 		self.isConnected = False
-		self.server_name = None
 
 	def remplieDict(self, line, dico):
 		lineList = line.split('=')
@@ -42,147 +42,131 @@ class Iocp:
 		
 
 
-	def connect(self,IP= None, Port= None):
-		""" This function is used to establish the connection, give it the IP and Port return server name if ok or None"""
-
-		if IP is not None:
-			self.IP = IP
-		if Port is not None:
-			self.Port = Port
-
-		print('IOCP: Essai de connexion sur',self.IP, self.Port,"......")
+	def connect(self,IP="", Port= 0):
+		""" This function is used to establish the connection, give it the IP and Port"""
+		if self.IP == "": self.IP = IP
+		if self.Port == 0: self.Port = Port
+		print('Try to connect to ',self.IP, self.Port,"......")
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.s.settimeout(2)
-
-		# try to connect to server
+		self.s.setblocking(0)
+		self.s.settimeout(2.0)
 		try:
 			self.s.connect((self.IP,self.Port))
-			server_name = self.s.recv(1024)
-			# if receive empty string
-			if (server_name == b' '):
-				self.isConnected = False
-				print('IOCP: Unable to receive server name')
-				return None
+			data = self.s.recv(1024)
 			self.isConnected = True
-			print("IOCP: Connexion avec",self.IP,"etablie")
-			server_name = server_name.decode()
-			serverList = server_name.split(':')
-			self.server_name = serverList[1]
-			return self.server_name
+			print("Connexion with ",self.IP,"established\n\n\n")
+			data = data.decode()
+			return 0
 		except:
-			print("IOCP: Erreur de connexion")
+			print("Connexion error")
 			self.isConnected = False
-			return None
+			return -1
 		
 		
-
+	def is_connected(self):
+		""" are we connected to the server ?"""
+		return self.isConnected
 
 
 	def register(self,register):
-		""" Register to the server with a list of variable as parameter and returna dict with initial values or None if unable to register"""
-
-		# if register is empty -> return None
+		""" Register to the server with a list of variable as parameter"""
 		if len(register) == 0 or self.isConnected == False:
-			return None
+			print("Unable to register")
+			return -1
+
+
 		self.listRegister = register
-
-
-
-		# the register line to send to server								
-		registerLine = "Arn.Inicio:"
+		reg = "Arn.Inicio:"
 		for l in self.listRegister:
-			registerLine = registerLine + str(l) + ":"
-		registerLine = registerLine + "\r\n"
-		registerLine = registerLine.encode()
-
-		print('IOCP: try to register to ', self.server_name)
-		# try to send datas
-		try:
-			self.s.send(registerLine)
-		except:
-			print('IOCP: Unable to send register vars')
-			return None
-
-		# try to receive
-		data = None
-		try:
-			data = self.s.recv(1024)
-		except:
-			print('IOCP: Unable to receive initials datas')
-			return None
-
-		data = data.decode()
+			reg = reg + str(l) + ":"
+		reg = reg + "\r\n"
+		print("Registration of: " + reg)
+		reg = reg.encode()
 		
+		# send data to register
+		self.s.send(reg)
+		time.sleep(1)
 
-		# Split received data separated by :
-		tmpList = data.split(':') 	
-		# remove first index Arn.resp:
-		tmpList.pop(0)
-		# remove last index :CRLF
-		tmpList.pop()   # ATTENTION
-
-		print('IOCP: Datas registered to serveur: ',tmpList)
+		# values from registration
+		reg = self.recvData()
+		print("Registration from server: " + str(reg))
+		return reg
 		
-		dataDict = {}
-		for n in tmpList:
-			self.remplieDict(n, dataDict)
-
-		# set non blocking mode 
-		self.s.settimeout(0.0)
-
-		return dataDict
 
 
 	def recvData(self):
-		""" Look if something comes from server, the function returns a dict [number][var] or None"""
+		""" Look if something comes from server, the function returns a dict ["number"]["var"] or -1"""
 		dataDict = {}
-		
-		# test if new data comes
+
 		try:
 			data = self.s.recv(1024)
 		except:
-			return None
+			return -1
+		
 
-		# test data len
+		# test server connexion""
 		if(len(data) == 0):
-			return None
-
-		#print(data)
-		data = data.decode() 
-		tmpList = data.split(':') 	
-		tmpList.pop(0)
-		tmpList.pop()   # ATTENTION
-		for n in tmpList:
-			self.remplieDict(n, dataDict)
+			print("No connexion with server")
+			return -1
+			
+		data = data.decode()
+		data = data.split("\n")
+		for i in data:
+			if i.find("\r") != -1:
+				if i.find("A") != -1:
+					tmpList = i.split(':') 	
+					tmpList.pop(0)
+					tmpList.pop()   
+					for n in tmpList:
+						self.remplieDict(n, dataDict)
 
 
 		return dataDict
 
 
-	def recvRaw(self):
-		'''Test for fata receive and return None or data string'''
-		pass
 
 
+	def sendData(self,var,val):
+		""" If client want to mofify a registered variable var = char, val = char"""
 
-	def sendData():
-		""" If client want to mofify a registered variable"""
-		pass
+		resp = "Arn.Resp:"
+		resp = resp + var + "=" + val + ":" + "\r\n"
+		resp = resp.encode()
+		
+		if self.isConnected:
+	 		self.s.sendall(resp)
+		print("Sending: " + str(resp))
+		return
+
 	
 	def close(self):
 		""" This function will close the connection"""
-		print("IOCP: Connexion closed")
+		print("Connexion with server closed")
 		if self.isConnected:
 			fin = "Arn.Fin:\r\n"
 			fin = fin.encode()
 			self.s.send(fin)
 			self.s.close()
-
-
-#------------------------------------------------------------
+			self.isConnected = False
 
 
 
+def main():
+	print("main")
+	iocp = Iocp()
+	while iocp.connect("127.0.0.1", 8090) == -1:
+		time.sleep(2)
+	iocp.register([500,501,502,503,510])
+	while 1:
+		print(iocp.recvData())
+		time.sleep(2)
+
+
+
+
+
+if __name__ == "__main__":
+	main()
 
 
 
